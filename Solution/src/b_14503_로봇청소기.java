9abd37d15105aae71e62792d0763a8c6ecf03b9f
정수빈 <wwaterbeen@gmail.com>
2021-05-31 19:27:55 +0900

    add init class

---
+++ Solution/src/b_14503_로봇청소기.java
@@ -0,0 +1,5 @@
+public class b_14503_로봇청소기 {
+    public static void main(String[] args) {
+        System.out.println("b_14503_로봇청소기");
+    }
+}
---
+++ Solution/src/b_7569_토마토.java
@@ -0,0 +1,5 @@
+public class b_7569_토마토 {
+    public static void main(String[] args) {
+        System.out.println("b_7569_토마토");
+    }
+}
